543932d96f08bde435c0363de82a4d106f596494
kish-00 <59705198+kish-00@users.noreply.github.com>
2022-03-07 16:36:39 +0300

    add Exception

--- src/main/java/com/movies/app/Controller/Exception/ResourceNotFoundException.java
+++ src/main/java/com/movies/app/Controller/Exception/ResourceNotFoundException.java
@@ -1,13 +1,9 @@
-package com.movies.app.Controller.exception;
+package com.movies.app.Controller.Exception;
 
-
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
-import static org.apache.http.HttpStatus.SC_NOT_FOUND;
-
-@ResponseStatus(value = org.springframework.http.HttpStatus.NOT_FOUND)
+@ResponseStatus(value = NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
     public ResourceNotFoundException (String message){
         super(message);
