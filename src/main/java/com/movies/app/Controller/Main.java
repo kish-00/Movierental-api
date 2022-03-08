ab0340db6b3b0618bba084c5d64c7b5620e9e670
kish-00 <59705198+kish-00@users.noreply.github.com>
2022-03-08 15:24:19 +0300

    Update Main

--- src/main/java/com/movies/app/Controller/Main.java
+++ src/main/java/com/movies/app/Controller/Main.java
@@ -1,13 +1,13 @@
-SpringBootDemoApplication.java
-        package com.howtodoinjava.rest;
+package com.movies.app.Controller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootDemoApplication {
+public class Main{
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootDemoApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
+
 }
