a9a6d2dc19ead59c514fe06922488142f48d8971
kish-00 <59705198+kish-00@users.noreply.github.com>
2022-03-21 10:59:12 +0300

    update Security

--- src/main/java/com/movies/app/Controller/Security/WebSecurityConfig.java
+++ src/main/java/com/movies/app/Controller/Security/WebSecurityConfig.java
@@ -1,2 +1,22 @@
-package com.movies.app.Controller.Security;public class WebSecurityConfig {
+package com.movies.app.Controller.Security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
+                .anyRequest().authenticated();
+    }
 }
