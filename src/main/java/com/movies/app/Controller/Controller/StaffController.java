7573074fb9bb3b3489f9a0d438b66515f6ccb235
kish-00 <59705198+kish-00@users.noreply.github.com>
2022-04-05 11:03:26 +0300

    create an authentication endpoint

--- src/main/java/com/movies/app/Controller/Controller/StaffController.java
+++ src/main/java/com/movies/app/Controller/Controller/StaffController.java
@@ -1,22 +1,47 @@
 package com.movies.app.Controller.Controller;
 
+import com.movies.app.Controller.Model.AuthenticationRequest;
+import com.movies.app.Controller.Model.AuthenticationResponse;
 import com.movies.app.Controller.Model.Staff;
 import com.movies.app.Controller.Service.StaffService;
+import com.movies.app.Controller.Util.jwtUtil;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static org.springframework.http.ResponseEntity.ok;
+
 @RestController
+@AllArgsConstructor
 @RequestMapping(path = "/")
 public class StaffController {
 
-    private final StaffService staffService;
-
     @Autowired
-    public StaffController(StaffService staffService) {
-        this.staffService = staffService;
-    }
+    private final StaffService staffService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private jwtUtil jwtUtil;
+
+    @PostMapping("/authenticate")
+        public ResponseEntity<AuthenticationResponse> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
+            try {
+                authenticationManager.authenticate(
+                        new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword()));
+            } catch (BadCredentialsException e) {
+                throw new BadCredentialsException("Incorrect username or password ", e);
+            }
+            UserDetails userDetails = staffService.loadUserByUsername(authenticationRequest.getUsername());
+            String token = jwtUtil.generationToken(userDetails);
+            return ResponseEntity.ok(new AuthenticationResponse(token));
+        }
 
     //adds a staff
     @PostMapping(value = "/addStaff", consumes = "application/JSON")
